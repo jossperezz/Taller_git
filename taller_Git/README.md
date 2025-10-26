5b44b7de58cec3bd6fa06620d2c4303b06e26f60
jossperezz <jcperezc@ufpso.edu.co>
2025-10-26 17:27:21 -0500

    aqui se confirman los cambios

---
+++ taller_Git/README.md
@@ -0,0 +1,2 @@
+hola, abro este proyecto para guardarlo en mi repo como prueba de exito
+Joseph Perez 192464
---
+++ taller_Git/src/texto.java
@@ -0,0 +1,7 @@
+
+public class texto {
+
+    public static void main(String[] args) {
+        System.out.println("hola, abro este proyecto para guardarlo en mi repo como prueba de exito");
+    }
+}
